f702928fd00947b1300050fe20dbcef38e28aaae
cody <cshaner04@gmail.com>
2021-03-29 16:11:02 -0500

    covert to JPA/add dependencies to pom/configure persistence

---
+++ src/main/java/com/cedardrone/models/User.java
@@ -0,0 +1,57 @@
+package com.cedardrone.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name="user")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer userId;
+	@Size(min=2, max=25)
+	private String username;
+	@NotEmpty
+	private String email;
+	@NotEmpty
+	private String password;
+	
+	public User() {}
+	
+	public User(String username, String email, String password) {
+		this.username = username;
+		this.email = email;
+		this.password = password;
+	}
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
